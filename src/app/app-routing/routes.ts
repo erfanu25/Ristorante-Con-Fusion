8d06d842fbe93236a175819f5ba91a390e2d9f9e
Erfan <mderfan2@gmail.com>
2020-05-10 10:39:41 +0600

    assignment2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -7,9 +7,10 @@ import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
-  { path: 'home',  component: HomeComponent },
-  { path: 'menu',     component: MenuComponent },
-  { path: 'contactus',     component: ContactComponent },
-  { path: 'dishdetail/:id',     component: DishdetailComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'contactus', component: ContactComponent },
+  { path: 'aboutus', component: AboutComponent },
+  { path: 'dishdetail/:id', component: DishdetailComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,14 +12,16 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from './services/dish.service';
-import { PromotionService } from './services/promotion.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -43,8 +45,11 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [DishService,
-    PromotionService],
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
